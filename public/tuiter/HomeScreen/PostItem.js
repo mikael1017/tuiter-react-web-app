aea72698cbbe873c594dac0f1c92b6b6926725ed
Jaewoo <jaewoocho1017@gmail.com>
2023-02-17 12:22:54 -0800

    finished challenge part 1

--- public/tuiter/HomeScreen/PostItem.js
+++ public/tuiter/HomeScreen/PostItem.js
@@ -1,7 +1,7 @@
 export default function PostItem(post) {
   return `
-                <div class="row px-2">
-                  <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2 align-self-center">
+                <div class="row px-2 border-top border-secondary py-2">
+                  <div class=" col-xxl-1 col-xl-1 col-lg-1 col-md-1 col-sm-1">
                     <img
                     src="${post.avatarIcon}"
                     class="rounded-circle"
@@ -10,15 +10,62 @@ export default function PostItem(post) {
                     />
                   </div>
 
-                  <div class="col-xxl-10 col-xl-10 col-lg-10 col-md-10 col-sm-10">
-                    <div class="topic text-secondary">${post.topic}</div>
-                    <div class="name fw-bold">
-                      ${post.userName} <i class="fas fa-circle-check"></i>
-                      <span class="text-secondary fw-normal">- ${post.time}</span>
+                  <div class="col-xxl-10 col-xl-10 col-lg-10 col-md-10 col-sm-10 ms-3">
+                    <div class="img-post-container">
+                      <div class="fw-bold">
+                        ${post.id} <i class="fas fa-circle-check"></i>
+                        <span class="text-secondary fw-normal">@${post.handle} - ${post.time}</span>
+                      </div>
+                      <div class="">
+                        ${post.tweet}
+                      </div>
+                      <div class="image-container">
+                        <img
+                        class="border border-secondary rounded-top"
+                        src="${post.postImg}"
+                        width="462.66px"
+                        height="264px"
+                        />
+                      </div>
+                      <div class="img-description ps-2 border border-top-0 border-secondary rounded-bottom">
+                        <div class="img-title">
+                          ${post.imgTitle}
+                        </div>
+                        <div class="img-info text-secondary">
+                          ${post.imgDesc}
+                        </div>
+                        <div class="img-link text-secondary">
+                          
+                          ${post.postLink}
+                        </div>
+                      </div>
                     </div>
-                    <div class="desc fw-bold">
-                    ${post.title}
+                    <div class="action-row d-flex justify-content-between mt-2">
+                      <div>
+                        <a class="text-secondary" href="#">
+                          <i class="fa-regular fa-comment"></i>
+                          <span class="wd-num">${post.comments}</span>
+                        </a>
+                      </div>
+                      <div>
+                        <a class="text-secondary" href="#"
+                          ><i class="fa-solid fa-retweet"></i>
+                          <span class="wd-num">${post.retweets}</span></a
+                        >
+                      </div>
+
+                      <!-- red -->
+                      <a class="text-secondary" href="#">
+                        <i class="fa-solid fa-heart"></i>
+                        <span class="wd-num">${post.likes}</span>
+                      </a>
+          
+                      <a class="text-secondary" href="#">
+                        <i class="fa-solid fa-arrow-up-from-bracket"></i>
+                      </a>
+                      <div></div>
                     </div>
+                    
                   </div>
                 </div>
               `;
--- public/tuiter/HomeScreen/posts.js
+++ public/tuiter/HomeScreen/posts.js
@@ -4,8 +4,13 @@ const posts = [
     id: "Elon Musk",
     handle: "elonmusk",
     time: "23h",
-    tweet: "Amazing show about @Inspiration4x mission!",
+    tweet: 'Amazing show about <a href="#">@Inspiration4x</a> mission!',
     postImg: "../../images/countdown.jpeg",
+    imgTitle:
+      "Countdown: Inspiration4 Mission to Space | Netflix Official Site",
+    imgDesc:
+      "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space ...",
+    postLink: '<i class="fas fa-link"></i>netflix.com',
     comments: "4.2K",
     retweets: "3.5K",
     likes: "37.5K",
@@ -16,8 +21,11 @@ const posts = [
     handle: "New York Post",
     time: "23h",
     tweet:
-      "Grimes seen reading Karl M arx following split with world's richest man Elon Musk trib.al/nx2Gfaq",
+      'Grimes seen reading Karl M arx following split with world\'s richest man Elon Musk <a href="#">trib.al/nx2Gfaq</a>',
     postImg: "../../images/post2.webp",
+    imgTitle: "",
+    imgDesc: "",
+    postLink: "",
     comments: "965",
     retweets: "2.4K",
     likes: "4K",
