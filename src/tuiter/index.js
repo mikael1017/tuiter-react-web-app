834bd7f1418583dc1aa5937b0284f88366630d69
Jaewoo <jaewoocho1017@gmail.com>
2023-02-22 12:12:43 -0800

    completed 3.1

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import Nav from "../nav";
+import NavigationSidebar from "./NavigationSidebar";
 function Tuiter() {
   return (
     <div>
       <Nav />
+      <NavigationSidebar active="home" />
       <h1>Tuiter</h1>
     </div>
   );
